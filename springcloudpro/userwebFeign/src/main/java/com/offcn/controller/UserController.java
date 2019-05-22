9a0081fe935b1fe82cbcf00f98b798fbb278b145
guoyongsheng <guoys_6885@163.com>
2019-05-22 10:04:04 +0800

    完成调用组件 Feign+开启调用日志

---
+++ springcloudpro/userwebFeign/src/main/java/com/offcn/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.offcn.controller;
+
+import com.offcn.entity.User;
+import com.offcn.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    //向数据库增加信息
+    @RequestMapping("/add")
+    public String saveInfo(User user){
+        userService.saveInfo(user);
+        return "redirect:/user/list";
+    }
+
+    @RequestMapping("/toAdd")
+    public String toAdd(){
+        return "userAdd";
+    }
+
+
+    //显示全部信息列表 跳转页面
+    @RequestMapping("list")
+    public String getAll(Model model){
+        Map<String,Object> map = userService.findAll();
+        model.addAttribute("page",map.get("list"));
+        model.addAttribute("version",map.get("version"));
+        return "list";
+    }
+
+    //查询一条数据
+    @RequestMapping("/toEdit/{id}")
+    public String toEdit(@PathVariable("id") Long id,Model model){
+          User one = userService.findOne(id);
+          model.addAttribute("user",one);
+          return "userEdit";
+    }
+
+    //修改数据
+    @RequestMapping("edit")
+     public String Edit(User user){
+        userService.updateInfo(user);
+        return "redirect:/user/list";
+     }
+    //删除数据
+    @RequestMapping("delete/{id}")
+    public String delete(@PathVariable("id") Long id){
+        userService.deleteInfo(id);
+        return "redirect:/user/list";
+    }
+
+}
---
+++ springcloudpro/userwebFeign/src/main/java/com/offcn/service/UserService.java
@@ -0,0 +1,33 @@
+package com.offcn.service;
+
+import com.offcn.config.FeignConfig;
+import com.offcn.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@FeignClient(value = "USERPROVIDER",configuration = FeignConfig.class)
+public interface UserService {
+
+    //向数据库增加信息
+    @PostMapping("/users/")
+    public void saveInfo(@RequestBody User user);
+
+    //查询全部的数据
+    @GetMapping("/users/")
+    public Map<String,Object> findAll();
+
+    //查询一条数据
+    @GetMapping("/users/{id}")
+    public User findOne(@PathVariable("id") Long id);
+
+    //修改数据
+    @PutMapping("/")
+    public void updateInfo(User user);
+
+    //删除信息
+    @DeleteMapping("/")
+    public void deleteInfo(Long id);
+
+}
